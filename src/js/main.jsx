7fbebb3d506ce72ac842b7aa2930ad85f03b12bb
Jaime <jaime.baker@gmail.com>
2018-10-28 15:31:16 -0400

    Initiial commit for stuff

---
+++ src/js/main.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import {render} from "react-dom"
+// import {Scorpio} from "scorpio"
+// import ProxyClient from "proxy-client"
+
+// Attach libraries to the window
+// window.proxyClient = new ProxyClient();
+// window.$ = window.jQuery = require('jquery');
+// window.Tether = require('tether');
+// window.Bootstrap = require('bootstrap');
+// window.io = require('socket.io-client');
+// window.moment = require('moment');
+// window.isElectron = window.navigator.userAgent.indexOf("Electron") !== -1;
+// window.engine = require('../../node_modules/iracing-shared-js/vendor/coherent-libs/coherentui/coherent.js')
+// window.accounting = require('accounting');
+// window.log = require("ir-bunyan");
+// window.c3 = require("c3")
+// window.d3 = require("d3")
+
+// render((<Scorpio />),document.getElementById("main"));
